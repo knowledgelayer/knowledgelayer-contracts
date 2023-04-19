f38d1a910c3070ef01f0578c77ab805d4671775f
mattiapomelli <mattiapomelli@gmail.com>
2023-04-19 19:34:55 +0200

    Update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,11 +11,29 @@ async function main() {
   const balance = await ethers.provider.getBalance(deployer.address);
   console.log('Balance: ', ethers.utils.formatEther(balance));
 
+  const KnowledgeLayerPlatformID = await ethers.getContractFactory('KnowledgeLayerPlatformID');
+  const knowledgeLayerPlatformId = await KnowledgeLayerPlatformID.deploy();
+  await knowledgeLayerPlatformId.deployed();
+
+  console.log('Deployed KnowledgeLayerPlatformID at', knowledgeLayerPlatformId.address);
+  setDeploymentProperty(
+    network,
+    ConfigProperty.KnowledgeLayerPlatformID,
+    knowledgeLayerPlatformId.address,
+  );
+
+  const KnowledgeLayerID = await ethers.getContractFactory('KnowledgeLayerID');
+  const knowledgeLayerId = await KnowledgeLayerID.deploy(knowledgeLayerPlatformId.address);
+  await knowledgeLayerId.deployed();
+
+  console.log('Deployed KnowledgeLayerID at', knowledgeLayerId.address);
+  setDeploymentProperty(network, ConfigProperty.KnowledgeLayerID, knowledgeLayerId.address);
+
   const KnowledgeLayerCourse = await ethers.getContractFactory('KnowledgeLayerCourse');
-  const knowledgeLayerCourse = await KnowledgeLayerCourse.deploy();
+  const knowledgeLayerCourse = await KnowledgeLayerCourse.deploy(knowledgeLayerId.address);
   await knowledgeLayerCourse.deployed();
 
-  console.log('Deployed KnowledgeLayer at', knowledgeLayerCourse.address);
+  console.log('Deployed KnowledgeLayerCourse at', knowledgeLayerCourse.address);
   setDeploymentProperty(network, ConfigProperty.KnowledgeLayerCourse, knowledgeLayerCourse.address);
 }
 
