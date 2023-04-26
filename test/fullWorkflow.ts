0c76eb46cb9d2d4f230179cda4f00626d018b80a
mattiapomelli <mattiapomelli@gmail.com>
2023-04-26 16:39:34 +0300

    Don't upload course data to ipfs in tests

--- test/fullWorkflow.ts
+++ test/fullWorkflow.ts
@@ -7,7 +7,6 @@ import {
   KnowledgeLayerID,
   KnowledgeLayerPlatformID,
 } from '../typechain-types';
-import uploadToIPFS from '../utils/uploadToIpfs';
 import deploy from '../utils/deploy';
 import { FEE_DIVIDER, MintStatus, PROTOCOL_INDEX } from '../utils/constants';
 import { expect } from 'chai';
@@ -33,14 +32,7 @@ describe('Full Workflow', () => {
 
   const courseId = 1;
   const coursePrice = 100;
-  const courseData = {
-    title: 'My cool course',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, velit rerum reprehenderit natus omnis eligendi iure amet fugit assumenda cumque id ad qui quos alias odit iusto provident. Nostrum accusamus quae iure quod maiores!',
-    image:
-      'https://yvgbeqzuvfqmewtltglq.supabase.co/storage/v1/object/public/public/16814021907992.webp',
-    videoPlaybackId: 'a915y3226a68zhp7',
-  };
+  const courseDataUri = 'QmVFZBWZ9anb3HCQtSDXprjKdZMxThbKHedj1on5N2HqMf';
 
   before(async () => {
     [deployer, alice, bob, carol, dave] = await ethers.getSigners();
@@ -63,12 +55,9 @@ describe('Full Workflow', () => {
     await knowledgeLayerID.connect(bob).mint(PROTOCOL_INDEX, 'bob__');
 
     // Alice creates a course
-    const uri = await uploadToIPFS(courseData);
-    if (!uri) throw new Error('Failed to upload to IPFS');
-
     await knowledgeLayerCourse
       .connect(alice)
-      .createCourse(aliceId, carolPlatformId, coursePrice, uri);
+      .createCourse(aliceId, carolPlatformId, coursePrice, courseDataUri);
   });
 
   describe('Buy course', async () => {
--- test/knowledgeLayerCourse.ts
+++ test/knowledgeLayerCourse.ts
@@ -7,7 +7,6 @@ import {
   KnowledgeLayerID,
   KnowledgeLayerPlatformID,
 } from '../typechain-types';
-import uploadToIPFS from '../utils/uploadToIpfs';
 import deploy from '../utils/deploy';
 import { MintStatus } from '../utils/constants';
 
@@ -23,17 +22,9 @@ describe('KnowledgeLayerCourse', () => {
     knowledgeLayerPlatformID: KnowledgeLayerPlatformID,
     knowledgeLayerCourse: KnowledgeLayerCourse;
 
-  const courseData = {
-    title: 'My cool course',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, velit rerum reprehenderit natus omnis eligendi iure amet fugit assumenda cumque id ad qui quos alias odit iusto provident. Nostrum accusamus quae iure quod maiores!',
-    image:
-      'https://yvgbeqzuvfqmewtltglq.supabase.co/storage/v1/object/public/public/16814021907992.webp',
-    videoPlaybackId: 'a915y3226a68zhp7',
-  };
-
   const courseId = 1;
   const coursePrice = 100;
+  const courseDataUri = 'QmVFZBWZ9anb3HCQtSDXprjKdZMxThbKHedj1on5N2HqMf';
 
   before(async () => {
     [deployer, alice, bob, carol] = await ethers.getSigners();
@@ -54,17 +45,11 @@ describe('KnowledgeLayerCourse', () => {
   });
 
   describe('Create course', async () => {
-    let dataUri: string;
-
     before(async () => {
-      const uri = await uploadToIPFS(courseData);
-      if (!uri) throw new Error('Failed to upload to IPFS');
-      dataUri = uri;
-
       // Alice creates a course
       const tx = await knowledgeLayerCourse
         .connect(alice)
-        .createCourse(aliceId, carolPlatformId, coursePrice, dataUri);
+        .createCourse(aliceId, carolPlatformId, coursePrice, courseDataUri);
       await tx.wait();
     });
 
@@ -73,7 +58,7 @@ describe('KnowledgeLayerCourse', () => {
       expect(course.ownerId).to.equal(aliceId);
       expect(course.platformId).to.equal(carolPlatformId);
       expect(course.price).to.equal(coursePrice);
-      expect(course.dataUri).to.equal(dataUri);
+      expect(course.dataUri).to.equal(courseDataUri);
     });
   });
 
